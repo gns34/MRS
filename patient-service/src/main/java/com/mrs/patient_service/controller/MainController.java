401b6416178102a9ad450667dfcdcad4938b22b9
Gopi Nath <gnsgns199@gmail.com>
2025-09-17 23:35:53 +0530

    Started

--- patient-service/src/main/java/com/mrs/patient_service/controller/MainController.java
+++ patient-service/src/main/java/com/mrs/patient_service/controller/MainController.java
@@ -1,13 +1,16 @@
 package com.mrs.patient_service.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mrs.patient_service.model.Patient;
@@ -22,8 +25,26 @@ public class MainController {
 	public List<Patient> getAllUserHome(){
 		return patientService.getAllPatients();
 	}
+	@PostMapping("/byid")
+	public Optional<Patient> getPatientById(@RequestBody Patient patient) {
+		Long id= (long) patient.getId();
+		return patientService.getPatientById(id);
+		
+	}
+	
 	@PostMapping("/new")
 	public Patient newPatient(@RequestBody Patient patient) {
 		return patientService.savePatient(patient);
 	}
+	@PutMapping("/update")
+	public Patient updatePatient(@RequestBody Patient patient) {
+		
+		return patientService.updatePatient(patient);
+	}
+	@DeleteMapping("/delete")
+	public String deletePatient() {
+		System.out.println("Deleted");
+		return "Deleted";
+	}
+	
 }
